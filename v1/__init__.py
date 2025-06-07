b1318210ed2d704f831306cbb62a025baa96b87f
Wiraama <mawira01@students.uonbi.ac.ke>
2025-06-07 08:52:20 +0300

    update

--- v1/__init__.py
+++ v1/__init__.py
@@ -2,14 +2,17 @@ from flask import Flask
 from v1.routes.main import bot
 from v1.views.gig_dist import add_data
 from v1.extension import Base, engine
+from datetime import datetime
 
 def create_bot():
     
     app = Flask(__name__)
     
-    #Base.metadata.drop_all(engine)
-    #Base.metadata.create_all(engine)
-    #add_data()
+    Base.metadata.create_all(engine)
+    now = datetime.now()
+    if now.hour == 0 and now.minute == 0:
+        Gigs.__table__.drop(bind=engine)
+        add_data()
     
     app.register_blueprint(bot)
     
